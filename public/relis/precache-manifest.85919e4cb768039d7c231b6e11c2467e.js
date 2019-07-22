fcaa78716cd30d2979c4f07a57f8c37da877c52e
José Abarca Saavedra <jose@smart.rancagua.cl>
2019-07-22 17:25:20 -0400

    remove public folder on gitignore file

---
+++ public/relis/precache-manifest.85919e4cb768039d7c231b6e11c2467e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3700c9dc871bf39deece0126cd5c180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21b96ae506656f9e89d",
+    "url": "/static/css/main.cffdd757.chunk.css"
+  },
+  {
+    "revision": "07c34be4dd45c5f5053b",
+    "url": "/static/js/2.87163d5f.chunk.js"
+  },
+  {
+    "revision": "c21b96ae506656f9e89d",
+    "url": "/static/js/main.5cbb42ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
